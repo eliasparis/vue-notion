450067dc23486b77e1e84421f9d933621de913c0
elias.a.pintos.arias <elias.pintos.aris@gmail.com>
2022-04-12 13:01:32 +0100

    Image rendering improvement

--- src/blocks/helpers/image.vue
+++ src/blocks/helpers/image.vue
@@ -30,24 +30,6 @@ import { Blockable, blockComputed } from "@/lib/blockable";
 export default {
   extends: Blockable,
   name: "NotionImage",
-  data() {
-    return {
-      windowWidth: window.innerWidth
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      window.addEventListener("resize", this.onResize);
-    });
-  },
-  beforeDestroy() {
-    window.removeEventListener("resize", this.onResize);
-  },
-  methods: {
-    onResize() {
-      this.windowWidth = window.innerWidth;
-    }
-  },
   computed: {
     ...blockComputed,
     hasImageComponent() {
@@ -61,11 +43,8 @@ export default {
       };
     },
     style() {
-      const margin = (this.f.block_width - 1024) / 2;
-
       const base = {
         width: `${this.f.block_width}px`,
-        "max-width": "100%",
         position: "relative",
         margin: "0 auto"
       };
@@ -73,14 +52,9 @@ export default {
       if (this.f.block_page_width) {
         base.width = "100%";
         base["margin-left"] = "auto";
-      } else {
-        base["max-width"] = "98vw";
-        base["margin-left"] = `-${margin}px`;
-        if (this.windowWidth < this.f.block_width) {
-          base["margin-left"] = `calc((98vw - 1024px)/2 * -1)`;
-          base.width = "98vw";
-        }
+        base.maxWidth = "804px";
       }
+
       return base;
     }
   }
--- src/blocks/text.vue
+++ src/blocks/text.vue
@@ -1,17 +1,31 @@
 <template>
-  <p v-if="properties" :class="['notion-text', blockColorClass()]">
-    <NotionTextRenderer :text="title" v-bind="pass" />
-  </p>
-  <div v-else class="notion-blank">&nbsp;</div>
+  <fragment v-if="!isMenuItem">
+    <p v-if="properties" :class="['notion-text', blockColorClass()]">
+      <NotionTextRenderer :text="title" v-bind="pass" />
+    </p>
+    <div v-else class="notion-blank">&nbsp;</div>
+  </fragment>
 </template>
 
 <script>
 import { Blockable } from "@/lib/blockable";
 import NotionTextRenderer from "@/blocks/helpers/text-renderer";
+import fragment from "@/blocks/helpers/fragment";
 
 export default {
   extends: Blockable,
   name: "NotionText",
-  components: { NotionTextRenderer },
+  components: { NotionTextRenderer, fragment },
+  beforeMount() {
+    if (this.isMenuItem) {
+      this.$root.$emit("side-menu", this.title[0][0]);
+    }
+  },
+  computed: {
+    isMenuItem() {
+      const { title } = this;
+      return title && title[0] && title[0][0] && title[0][0].includes("$");
+    }
+  }
 };
 </script>
