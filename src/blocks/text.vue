ce4796de563e92ce65ccb3d2d84fa961c802363e
elias.a.pintos.arias <elias.pintos.aris@gmail.com>
2022-04-18 14:41:22 +0100

    Adding masive callouts

--- src/blocks/text.vue
+++ src/blocks/text.vue
@@ -1,11 +1,12 @@
 <template>
-  <fragment v-if="!isMenuItem">
+  <span v-if="isMenuItem" :id="pass.contentId" />
+  <span v-else-if="isMasiveStarter" class="notion-masive" />
+  <fragment v-else>
     <p v-if="properties" :class="['notion-text', blockColorClass()]">
       <NotionTextRenderer :text="title" v-bind="pass" />
     </p>
     <div v-else class="notion-blank">&nbsp;</div>
   </fragment>
-  <span v-else :id="pass.contentId" />
 </template>
 
 <script>
@@ -26,6 +27,10 @@ export default {
     isMenuItem() {
       const { title } = this;
       return title && title[0] && title[0][0] && title[0][0].includes("$");
+    },
+    isMasiveStarter() {
+      const { title } = this;
+      return title && title[0] && title[0][0] && title[0][0].includes("##");
     }
   }
 };
